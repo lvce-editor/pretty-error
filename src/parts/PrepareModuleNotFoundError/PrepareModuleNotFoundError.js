798fc00dc825a9507ed3b97496f290f3e554d70c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-10 11:59:54 +0100

    refactor: move module not found error into separate file (#24)

---
+++ src/parts/PrepareModuleNotFoundError/PrepareModuleNotFoundError.js
@@ -0,0 +1,54 @@
+import { codeFrameColumns } from '@babel/code-frame'
+import { readFileSync } from 'node:fs'
+import * as EncodingType from '../EncodingType/EncodingType.js'
+import * as JoinLines from '../JoinLines/JoinLines.js'
+import * as SplitLines from '../SplitLines/SplitLines.js'
+
+const RE_MODULE_NOT_FOUND_STACK =
+  /Cannot find package '([^']+)' imported from (.+)$/
+
+export const prepareModuleNotFoundError = (error) => {
+  const { message } = error
+  const match = message.match(RE_MODULE_NOT_FOUND_STACK)
+  if (!match) {
+    return {
+      message,
+      stack: error.stack,
+      codeFrame: '',
+    }
+  }
+  const notFoundModule = match[1]
+  const importedFrom = match[2]
+  const rawLines = readFileSync(importedFrom, EncodingType.Utf8)
+  let line = 0
+  let column = 0
+  const splittedLines = SplitLines.splitLines(rawLines)
+  for (let i = 0; i < splittedLines.length; i++) {
+    const splittedLine = splittedLines[i]
+    const index = splittedLine.indexOf(notFoundModule)
+    if (index !== -1) {
+      line = i + 1
+      column = index
+      break
+    }
+  }
+  const location = {
+    start: {
+      line,
+      column,
+    },
+  }
+  const codeFrame = codeFrameColumns(rawLines, location)
+  const stackLines = SplitLines.splitLines(error.stack)
+  const newStackLines = [
+    stackLines[0],
+    `    at ${importedFrom}:${line}:${column}`,
+    ...stackLines.slice(1),
+  ]
+  const newStack = JoinLines.joinLines(newStackLines)
+  return {
+    message,
+    stack: newStack,
+    codeFrame,
+  }
+}
