48661b41b83273015d5bb89cbf405a409fd01317
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-21 13:45:34 +0200

    feature: add shared process on error test (#29)

--- test/PrettyError.test.ts
+++ test/PrettyError.test.ts
@@ -1052,3 +1052,96 @@ export const wrap = (port) => {
     at handleMessage (/packages/embeds-worker/src/parts/HandleIpc/HandleIpc.ts:14:33)
     at MessagePort.wrappedListener (/packages/embeds-worker/src/parts/IpcChildWithModuleWorkerAndMessagePort/IpcChildWithModuleWorkerAndMessagePort.ts:40:21)`)
 })
+
+test('prepare - sharedProcess.on is not a function', () => {
+  const error = new TypeError()
+  error.message = `sharedProcess.on is not a function`
+  error.stack = `TypeError: sharedProcess.on is not a function
+    at Module.launchSharedProcess (/test/packages/main-process/src/parts/LaunchSharedProcess/LaunchSharedProcess.js:49:17)
+    at async Module.hydrate (/test/packages/main-process/src/parts/App/App.js:102:3)
+    at async Module.main (/test/packages/main-process/src/parts/Main/Main.js:16:3)`
+  // @ts-ignore
+  fs.readFileSync.mockImplementation(() => {
+    return `import { MessageChannelMain } from 'electron'
+import * as ExitCode from '../ExitCode/ExitCode.js'
+import * as GetSharedProcessArgv from '../GetSharedProcessArgv/GetSharedProcessArgv.js'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as IpcChild from '../IpcChild/IpcChild.js'
+import * as IpcChildType from '../IpcChildType/IpcChildType.js'
+import * as IpcParent from '../IpcParent/IpcParent.js'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+import * as Logger from '../Logger/Logger.js'
+import * as Performance from '../Performance/Performance.js'
+import * as PerformanceMarkerType from '../PerformanceMarkerType/PerformanceMarkerType.js'
+import * as Platform from '../Platform/Platform.js'
+import * as Process from '../Process/Process.js'
+import * as SharedProcessState from '../SharedProcessState/SharedProcessState.js'
+
+const handleChildError = (error) => {
+  Process.exit(ExitCode.Error)
+}
+
+const handleChildExit = (event) => {
+  const code = event.data
+  Logger.info(\`[main process] shared process exited with code \${code}\`)
+  Process.exit(code)
+}
+
+const handleChildDisconnect = () => {
+  Logger.info('[main process] shared process disconnected')
+}
+
+export const launchSharedProcess = async ({ method, env = {} }) => {
+  Performance.mark(PerformanceMarkerType.WillStartSharedProcess)
+  const sharedProcessPath = Platform.getSharedProcessPath()
+  const execArgv = GetSharedProcessArgv.getSharedProcessArgv()
+  const fullEnv = {
+    ...process.env,
+    ...env,
+  }
+  if (!Platform.isProduction) {
+    // fullEnv['NODE_OPTIONS'] = '--import=@swc-node/register/esm-register'
+  }
+  const sharedProcess = await IpcParent.create({
+    method,
+    env: fullEnv,
+    argv: [],
+    execArgv,
+    path: sharedProcessPath,
+    name: 'shared-process',
+  })
+  sharedProcess.on('error', handleChildError)
+  sharedProcess.on('exit', handleChildExit)
+  sharedProcess.on('disconnect', handleChildDisconnect)
+  HandleIpc.handleIpc(sharedProcess)
+
+  // create secondary ipc to support transferring objects
+  // from shared process to main process
+  const { port1, port2 } = new MessageChannelMain()
+  const childIpc = await IpcChild.listen({
+    method: IpcChildType.ElectronMessagePort,
+    messagePort: port1,
+  })
+  HandleIpc.handleIpc(childIpc)
+  await JsonRpc.invokeAndTransfer(sharedProcess, [port2], 'HandleElectronMessagePort.handleElectronMessagePort', -5)
+  SharedProcessState.state.sharedProcess = sharedProcess
+  Performance.mark(PerformanceMarkerType.DidStartSharedProcess)
+  return sharedProcess
+}
+`
+  })
+  const prettyError = PrettyError.prepare(error)
+  expect(prettyError.type).toBe('TypeError')
+  expect(prettyError.message).toBe('sharedProcess.on is not a function')
+  expect(prettyError.codeFrame).toBe(`  47 |     name: 'shared-process',
+  48 |   })
+> 49 |   sharedProcess.on('error', handleChildError)
+     |                 ^
+  50 |   sharedProcess.on('exit', handleChildExit)
+  51 |   sharedProcess.on('disconnect', handleChildDisconnect)
+  52 |   HandleIpc.handleIpc(sharedProcess)`)
+  expect(prettyError.stack)
+    .toBe(`    at launchSharedProcess (/test/packages/main-process/src/parts/LaunchSharedProcess/LaunchSharedProcess.js:49:17)
+    at async hydrate (/test/packages/main-process/src/parts/App/App.js:102:3)
+    at async main (/test/packages/main-process/src/parts/Main/Main.js:16:3)`)
+})
