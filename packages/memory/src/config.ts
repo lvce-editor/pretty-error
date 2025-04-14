8e8013af92edb56b0676fcb8daa1e78b51ff7c93
Le Vivilet <levivlet@pm.me>
2025-04-14 18:29:11 +0200

    feature: add lerna

---
+++ packages/memory/src/config.ts
@@ -0,0 +1,7 @@
+import { join } from 'node:path'
+import { root } from './root.ts'
+
+
+export const instantiations = 4400
+
+export const instantiationsPath = join(root, 'packages', 'pretty-error')
