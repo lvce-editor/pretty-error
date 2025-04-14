82a6c75ff111b968c0ecd54be2d0289e5515f241
Le Vivilet <levivlet@pm.me>
2025-04-14 19:10:57 +0200

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,11 @@ export default [
       '@typescript-eslint/no-unused-vars': 'off',
       'prefer-destructuring': 'off',
       '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      'no-useless-escape': 'off',
+      'unicorn/error-message': 'off',
+      'jest/no-disabled-tests': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'no-console': 'off',
     },
   },
 ]
