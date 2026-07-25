7b42498f08c31978bf1036154e03fbe901e5571f
Le Vivilet <levivilet@pm.me>
2026-07-25 22:18:08 +0000

    feature: replace lerna with npm workspaces

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -16,17 +16,15 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x @types/node -x typescript`
-  SUB='All dependencies match the latest package versions'
-  if [[ "$OUTPUT" == *"$SUB"* ]]; then
-    echo "$OUTPUT"
-  else
-    rm -rf node_modules package-lock.json dist
-    npm install
-  fi
+  ncu -u -x @types/node -x typescript
 }
 
-updateDependencies &&
+                              updateDependencies &&
+cd packages/build          && updateDependencies && cd ../../ &&
+cd packages/memory         && updateDependencies && cd ../../ &&
+cd packages/pretty-error   && updateDependencies && cd ../../ &&
+
+npm install
 
 echo "Great Success!"
 
